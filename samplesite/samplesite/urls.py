b3d71ad53c6f5f0973cd4530e4dc3993e17d9b4e
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2025-02-17 14:31:52 +0300

    refactor: add include path

--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from bboard.views import index
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('bboard/', index)
-]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [path("admin/", admin.site.urls), path("bboard/", include("bboard.urls"))]
