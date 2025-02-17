7342afc0566d8963f99cfd4f79ba922460458704
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2025-02-17 17:11:23 +0300

    feat: register bboard in admin

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -2,8 +2,9 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Bb
 
+
 def index(request):
     template = loader.get_template("bboard/index.html")
-    bbs = Bb.objects.order_by("-published")
+    bbs = Bb.objects.all()
     context = {"bbs": bbs}
     return HttpResponse(template.render(context, request))
